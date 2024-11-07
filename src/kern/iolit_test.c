9c7dba8d72663842bc9fa8de2ce323beec3b66a6
Carlos Carretero <ccarr27@illinois.edu>
2024-11-06 20:20:01 -0600

    added comments to io.c and elf.c then made iolit_test.c and added it to the makefile

---
+++ src/kern/iolit_test.c
@@ -0,0 +1,125 @@
+#include "console.h"
+#include "thread.h"
+#include "device.h"
+#include "uart.h"
+#include "timer.h"
+#include "intr.h"
+#include "heap.h"
+#include "virtio.h"
+#include "halt.h"
+#include "elf.h"
+#include "fs.h"
+#include "string.h"
+#include "io.h"
+
+//           end of kernel image (defined in kernel.ld)
+extern char _kimg_end[];
+
+
+#define RAM_SIZE (8*1024*1024)
+#define RAM_START 0x80000000UL
+#define KERN_START RAM_START
+#define USER_START 0x80100000UL
+
+#define UART0_IOBASE 0x10000000
+#define UART1_IOBASE 0x10000100
+#define UART0_IRQNO 10
+
+#define VIRT0_IOBASE 0x10001000
+#define VIRT1_IOBASE 0x10002000
+#define VIRT0_IRQNO 1
+
+
+
+// static void shell_main(struct io_intf * termio);
+
+void main(void) {
+    struct io_intf * io;
+    void * mmio_base;
+    // int result;
+    int i;
+
+    console_init();
+    intr_init();
+    devmgr_init();
+    thread_init();
+    timer_init();
+
+    heap_init(_kimg_end, (void*)USER_START);
+
+    //           Attach NS16550a serial devices
+
+    for (i = 0; i < 2; i++) {
+        mmio_base = (void*)UART0_IOBASE;
+        mmio_base += (UART1_IOBASE-UART0_IOBASE)*i;
+        uart_attach(mmio_base, UART0_IRQNO+i);
+    }
+    
+    //           Attach virtio devices
+
+    for (i = 0; i < 8; i++) {
+        mmio_base = (void*)VIRT0_IOBASE;
+        mmio_base += (VIRT1_IOBASE-VIRT0_IOBASE)*i;
+        virtio_attach(mmio_base, VIRT0_IRQNO+i);
+    }
+
+    intr_enable();
+    timer_start();
+
+    struct io_lit * lit = kmalloc(sizeof(struct  io_lit));
+
+    int buffer[128];  // A memory buffer that will act like a "file"
+
+    const int message = 5;
+
+    console_printf("%d\n", message);
+
+
+    io = iolit_init(lit, buffer, sizeof(buffer));
+
+    console_printf("io init done \n");
+
+    //get the curr position
+    int curr_pos = io->ops->ctl(io, IOCTL_GETPOS, NULL);
+
+    console_printf("curr pos: %d\n", curr_pos);
+
+    //io write test
+    int yy = io->ops->write(io, &message, sizeof(int));
+
+    console_printf("io write done: %d \n", yy);
+
+    int recieve_buffer[128];
+
+    io->ops->read(io, recieve_buffer, sizeof(int));
+
+    // Print the first character in buffer to check if it was written correctly
+    console_printf("First character in buffer: %d\n", recieve_buffer[0]);
+
+
+    //get the curr position
+    curr_pos = io->ops->ctl(io, IOCTL_GETPOS, NULL);
+
+    console_printf("curr pos: %d\n", curr_pos);
+    
+
+    // // Read the message into another buffer and print it
+    // char message_receive[128] = {0};
+    // io->ops->read(io, message_receive, strlen(message));
+    // console_printf("Received message: %s\n", message_receive);
+
+    // char * message_recieve;
+
+    // // //io read test
+    // io->ops->read(io, message_recieve, sizeof(*message));
+
+    // console_printf("this is the magic recieved number: %s\n", message_recieve);
+
+
+
+    // int curr_pos = io->ops->ctl(io, IOCTL_GETPOS, NULL);
+
+    // console_printf("curr pos: %d\n", curr_pos);
+
+
+}
