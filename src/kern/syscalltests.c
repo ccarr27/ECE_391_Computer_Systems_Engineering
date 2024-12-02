4fb555818495b6e02647013f8a4acf72966c75da
Carlos Carretero <ccarr27@illinois.edu>
2024-12-02 17:52:32 -0600

    changes

--- src/kern/syscalltests.c
+++ src/kern/syscalltests.c
@@ -46,25 +46,24 @@
 
 int main() {
     thread_init();
+    procmgr_init();
+
     struct trap_frame temp_frame = {0};
 
-    char * testname = "msgout";
 
-    // msgout
+    const char *message = "Hello, World!";
 
-    switch (test):
-        case("msgout"):
-            const char *message = "Hello, World!";
+    temp_frame.x[TFR_A7] = SYSCALL_MSGOUT;
+    temp_frame.x[TFR_A0] = (uint64_t)message;
 
-            temp_frame.x[TFR_A7] = SYSCALL_MSGOUT;
-            temp_frame.x[TFR_A0] = (uint64_t)message;
+    // temp_frame.x[TFR_A7] = SYSCALL_EXIT;
+    // temp_frame.x[TFR_A0] = 0;       
 
-        case("exit"):
-            
+    // temp_frame.x[TFR_A7] = SYSCALL_IOCTL;
+    // temp_frame.x[TFR_A0] = 3;
+    // temp_frame.x[TFR_A0] = 100;
+    // temp_frame.x[TFR_A0] = (uint64_t) & temp_frame;
 
-
-
-    
 
     syscall(&temp_frame);
 
