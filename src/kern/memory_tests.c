f162af9fdf28bfc4fc3dd3c6046c82eac10a5230
Dash <dashpk2@illinois.edu>
2024-12-02 16:45:12 -0600

    memory test cases added

--- src/kern/memory_tests.c
+++ src/kern/memory_tests.c
@@ -98,9 +98,67 @@ void main(void) {
     
 
 
-    // TEST CASE: demonstrating page implementation by creating and mapping pages, then editing the memory inside of them
+    console_printf("\n============ Test cases ============\n");
 
-    // memory_alloc_and_map_page(vma+ (x * PAGE_SIZE) , rwxug_flags);
+    // TEST CASE: demonstrates successful paging implementation with repeated pointer 
+    //            arithmetic operations
+
+    console_printf("\nDemonstrating paging: \n");
+
+    void * allocatedPage1 = memory_alloc_and_map_page(USER_START_VMA+(2000 * PAGE_SIZE), PTE_R|PTE_W|PTE_U);
+    void * allocatedPage2 = memory_alloc_and_map_page(USER_START_VMA+(2002 * PAGE_SIZE), PTE_R|PTE_W|PTE_U);
+    void * allocatedPage3 = memory_alloc_and_map_page(USER_START_VMA+(2004 * PAGE_SIZE), PTE_R|PTE_W|PTE_U);
+
+    int * firstPage = (int *)allocatedPage1;
+    int * secondPage = (int *)allocatedPage2;
+    int * thirdPage = (int *)allocatedPage3;
+
+    *firstPage = 0x1234;
+    *secondPage = 0x5678;
+    *thirdPage = 0xfffe;
+
+    console_printf("First Page Value: 0x%x\n", *firstPage);
+    console_printf("Second Page Value: 0x%x\n", *secondPage);
+    console_printf("Third Page Initial Value: 0x%x\n", *thirdPage);
+
+    int * thirdPageModified = allocatedPage2 + PAGE_SIZE*2;
+    *thirdPageModified += 0x1;  
+
+    console_printf("Third Page Final Value: 0x%x\n", *thirdPage);
+
+    
+
+    
+    // TEST CASE: triggering various faults 
+
+    console_printf("\n\nTriggering faults:\n");
+    
+    console_printf("\nWriting kernel page from user mode: \n"); // Test 1
+    long * testKernelMem1 = 0x8000b000;
+    long * testKernelMem2 = 0x8000c000;
+    *testKernelMem1 = 0xabcd;
+
+    console_printf("\nReading kernel page from user mode: \n"); // Test 2
+    long * testKernelMem = 0x8000c000;
+    int tmp = *testKernelMem;
+    console_printf("Memory read from %p: %d\n", testKernelMem, tmp);
+
+    console_printf("\nReading from a page without read permissions:\n"); // Test 3
+    void * noReadPage = memory_alloc_and_map_page(USER_START_VMA+(128 * PAGE_SIZE), PTE_W);
+    int * readPage = (int *)noReadPage;
+    int * testRead = *readPage;
+    console_printf("Read Processed\n");
+
+    console_printf("\nWriting to a page without write permissions:\n"); // Test 4
+    void * noWritePage = memory_alloc_and_map_page(USER_START_VMA+(256 * PAGE_SIZE), PTE_R);
+    int * writePage = (int *)noWritePage;
+    *writePage = 0xbbbb;
+    console_printf("Write Processed\n");
+
+    console_printf("\nExecuting instructions from a page without execute permissions:s\n"); // Test 5
+
+
+
 
 
 }
